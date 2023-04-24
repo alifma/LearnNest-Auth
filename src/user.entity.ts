0713b6435c97a8a07adf12b12aae90fca7203e98
Alif Maulana <alif.maulana@citra.digital>
2023-04-24 10:06:34 +0700

    init and add users entity

---
+++ src/user.entity.ts
@@ -0,0 +1,16 @@
+const { Entity, PrimaryGeneratedColumn, Column } = require("typeorm");
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({unique: true})
+  email: string;
+
+  @Column()
+  password: string;
+}
